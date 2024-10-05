a541143f151a848f7265bd901d131210dc94a326
BellaP18 <145602974+BellaP18@users.noreply.github.com>
2024-10-05 10:02:01 -0400

    beginning of playermovement
    The beginning of playermovement code

---
+++ GranolaGameJam/Assets/Scripts/PlayerMovement_3.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_3 : MonoBehaviour
+{
+    //access to the player model/sprite for both
+    public GameObject mainCharacter;
+    public GameObject sideCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Moves the main character with AD, and spacebar to jump
+    /// </summary>
+    public void MainMovement()
+    {
+       
+    }
+
+   
+}
