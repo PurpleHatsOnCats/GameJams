ac596ec3cb51c0c41afffdca5fab7542d916f307
PurpleHatsOnCats <seth.vandivere@gmail.com>
2024-10-05 11:58:30 -0400

    Finished PickupController

--- GranolaGameJam/Assets/Scripts/Level_1/CharacterAttack.cs
+++ GranolaGameJam/Assets/Scripts/Level_1/CharacterAttack.cs
@@ -7,15 +7,33 @@ public class CharacterAttack : MonoBehaviour
     public GameObject ProjectilePrefab;
     public GameObject MeleePrefab;
 
-    public float ProjectileDamage;
+    public float ProjectileDamage = 1;
+    public float MeleeDamage = 2;
 
+    /// <summary>
+    /// Create a projectile object that moves the direction the player is facing
+    /// </summary>
     public void ProjectileAttack()
     {
-        GameObject projectile = Instantiate(ProjectilePrefab, transform.position, new Quaternion());
-        projectile.GetComponent<ProjectileController>().Initiate(
-            3, 
+        GameObject projectileObject = Instantiate(ProjectilePrefab, transform.position, new Quaternion());
+        projectileObject.GetComponent<ProjectileController>().Initiate(
+            3, // speed
             gameObject.GetComponent<CharacterMovement>().Direction, 
-            1,
+            ProjectileDamage,
+            4, // distance
+            gameObject.tag == "Player");
+    }
+    /// <summary>
+    /// Create a melee object that moves the direction the player is facing
+    /// </summary>
+    public void MeleeAttack()
+    {
+        GameObject meleeObject = Instantiate(MeleePrefab, transform.position, new Quaternion());
+        meleeObject.GetComponent<ProjectileController>().Initiate(
+            3, // speed
+            gameObject.GetComponent<CharacterMovement>().Direction,
+            MeleeDamage,
+            2, // distance
             gameObject.tag == "Player");
     }
 }
--- GranolaGameJam/Assets/Scripts/Level_1/PickupController.cs
+++ GranolaGameJam/Assets/Scripts/Level_1/PickupController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PickupController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int ItemID;
+    public float PickupRange;
+    public GameObject Player;
+    [HideInInspector]
+    public bool InRange;
+    public IntEvent ItemPickup;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 playerPosition = Player.transform.position;
+        Vector2 itemPosition = transform.position;
+        InRange = (playerPosition - itemPosition).magnitude <= PickupRange;
+    }
+    public void AttemptPickup()
+    {
+        if (InRange)
+        {
+            ItemPickup.Invoke(ItemID);
+        }
     }
 }
