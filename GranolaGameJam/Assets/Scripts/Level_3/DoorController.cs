48e6b014051f71a664c32387dc135708f22e5d8a
Fig Gamache <fdg3622@rit.edu>
2024-10-05 23:24:10 -0400

    added appropriate transtions from levels 2 and 3

--- GranolaGameJam/Assets/Scripts/Level_3/DoorController.cs
+++ GranolaGameJam/Assets/Scripts/Level_3/DoorController.cs
@@ -21,7 +21,7 @@ public class DoorController : MonoBehaviour
     {
         if (playerDoorEntered && otherDoor.GirlfriendDoorEntered)
         {
-            SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("cutscene3");
         }
     }
 
